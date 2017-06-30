f94aa22922ea235a27400b03d79833cf81c0da6b
radhajain <jainr@stanford.edu>
2017-06-30 16:23:16 -0700

    Swiping finished

--- app/views/congratsView/congratsView.js
+++ app/views/congratsView/congratsView.js
@@ -1,14 +1,6 @@
 var frameModule = require("ui/frame");
 
 
-// var gestures = require("ui/gestures");
-// var labelModule = require("ui/label");
-// var label = new labelModule.Label();
-// label.on(gestures.GestureTypes.swipe, function (args) {
-//     frameModule.topmost().navigate("views/navView/navView");
-// });
-
-
 exports.goToNavView = function() {
 	console.log("swiped")
   frameModule.topmost().navigate("views/navView/navView");
