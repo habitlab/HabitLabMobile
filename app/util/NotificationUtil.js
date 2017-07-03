b8e2ea492b442799bbf4a2d8c7abd9f54232c5f3
Sara <svalderr@stanford.edu>
2017-07-03 09:54:48 -0700

    Updates to notifications

--- app/util/NotificationUtil.js
+++ app/util/NotificationUtil.js
@@ -1,44 +1,44 @@
-// var Observable = require("data/observable").Observable;
-// var LocalNotifications = require("nativescript-local-notifications");
-// var dialogs = require("ui/dialogs");
+var Observable = require("data/observable").Observable;
+var LocalNotifications = require("nativescript-local-notifications");
+var dialogs = require("ui/dialogs");
  
-// var doAddOnMessageReceivedCallback = function() {
-//     LocalNotifications.addOnMessageReceivedCallback(
-//         function(notificationData) {
-//             dialogs.alert({
-//                 title: "Notification received",
-//                 message: "ID: " + notificationData.id +
-//                 "\nTitle: " + notificationData.title +
-//                 "\nBody: " + notificationData.body,
-//                 okButtonText: "Excellent!"
-//             });
-//         }
-//     );
-// };
+var doAddOnMessageReceivedCallback = function() {
+    LocalNotifications.addOnMessageReceivedCallback(
+        function(notificationData) {
+            dialogs.alert({
+                title: "Notification received",
+                message: "ID: " + notificationData.id +
+                "\nTitle: " + notificationData.title +
+                "\nBody: " + notificationData.body,
+                okButtonText: "Excellent!"
+            });
+        }
+    );
+};
  
-// exports.createViewModel = function() {
-//     var viewModel = new Observable();
+exports.createViewModel = function() {
+    var viewModel = new Observable();
  
-//     viewModel.id = 0;
-//     viewModel.title = "Test Title";
-//     viewModel.body = "Test Body";
-//     viewModel.ticker = "Test Ticker";
+    viewModel.id = 0;
+    viewModel.title = "Test Title";
+    viewModel.body = "Test Body";
+    viewModel.ticker = "Test Ticker";
  
-//     doAddOnMessageReceivedCallback();
+    doAddOnMessageReceivedCallback();
  
-//     viewModel.schedule = function() {
-//         LocalNotifications.schedule([{
-//             id: this.id,
-//             title: this.title,
-//             body: this.body,
-//             ticker: this.ticker,
-//             at: new Date(new Date().getTime() + (10 * 1000))
-//         }]).then(() => {
-//             console.log("Notification scheduled");
-//         }, (error) => {
-//             console.log("ERROR", error);
-//         });
-//     }
+    viewModel.schedule = function() {
+        LocalNotifications.schedule([{
+            id: this.id,
+            title: this.title,
+            body: this.body,
+            ticker: this.ticker,
+            at: new Date(new Date().getTime())
+        }]).then(() => {
+            console.log("Notification scheduled");
+        }, (error) => {
+            console.log("ERROR", error);
+        });
+    }
  
-//     return viewModel;
-// };
+    return viewModel;
+};
--- app/views/interventionsView/interventionsView.js
+++ app/views/interventionsView/interventionsView.js
@@ -1,16 +1,22 @@
-// var appsList = [];
-// var LocalNotifications = require("nativescript-local-notifications");
-// var createViewModel = require("NotificationUtil.js").createViewModel;
+var appsList = [];
+var LocalNotifications = require("nativescript-local-notifications");
+var createViewModel = require("~/util/NotificationUtil.js").createViewModel;
+var page;
 
-// exports.onSelectApp = function(index) {
+exports.onSelectApp = function(index) {
 
-// };
+};
 
-// exports.onNotify = function(args) {
-//   var page = args.object;
-//     LocalNotifications.requestPermission().then((granted) => {
-//         if(granted) {
-//             page.bindingContext = createViewModel();
-//         }
-//     })
-// };
+exports.onNavigate = function(args) {
+  page = args.object;
+    LocalNotifications.requestPermission().then((granted) => {
+        if(granted) {
+            console.log("GRANTED");
+            page.bindingContext = createViewModel();
+        }
+    })
+};
+
+exports.onNotify = function() {
+  page.bindingContext.schedule();
+};
