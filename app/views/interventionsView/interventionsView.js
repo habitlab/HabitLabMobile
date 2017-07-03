089a6f1842ee77e7ed935d91a660a5113e0e01c5
radhajain <jainr@stanford.edu>
2017-07-03 16:08:30 -0700

    All menu working

--- app/views/interventionsView/interventionsView.js
+++ app/views/interventionsView/interventionsView.js
@@ -5,18 +5,35 @@ var view = require("ui/core/view");
 var drawer;
 
 exports.pageLoaded = function(args) {
-}
+    var page = args.object;
+    drawer = page.getViewById("sideDrawer");
+};
 
 exports.onSelectApp = function(index) {
 
 };
 
-exports.pageLoaded = function(args) {
-    var page = args.object;
-    drawer = view.getViewById(page, "sideDrawer");
-};
+
 
 exports.toggleDrawer = function() {
     drawer.toggleDrawerState();
 };
 
+exports.goToProgress = function() {
+  frameModule.topmost().navigate("views/progressView/progressView");
+};
+
+
+exports.goToGoals = function() {
+  frameModule.topmost().navigate("views/goalsView/goalsView");
+};
+
+
+exports.goToSettings = function() {
+  frameModule.topmost().navigate("views/settingsView/settingsView");
+};
+
+
+exports.goToInterventions = function() {
+  frameModule.topmost().navigate("views/interventionsView/interventionsView");
+};
--- app/views/progressView/progressView.js
+++ app/views/progressView/progressView.js
@@ -12,3 +12,22 @@ exports.toggleDrawer = function() {
     drawer.toggleDrawerState();
 };
 
+
+exports.goToProgress = function() {
+  frameModule.topmost().navigate("views/progressView/progressView");
+};
+
+
+exports.goToGoals = function() {
+  frameModule.topmost().navigate("views/goalsView/goalsView");
+};
+
+
+exports.goToSettings = function() {
+  frameModule.topmost().navigate("views/settingsView/settingsView");
+};
+
+
+exports.goToInterventions = function() {
+  frameModule.topmost().navigate("views/interventionsView/interventionsView");
+};
