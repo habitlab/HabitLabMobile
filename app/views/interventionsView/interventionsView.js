8add63e57efe27c1520cf75870c2ee39ccbb9928
Sara <svalderr@stanford.edu>
2017-07-03 10:08:55 -0700

    got rid of notification stuff

--- app/views/interventionsView/interventionsView.js
+++ app/views/interventionsView/interventionsView.js
@@ -1,22 +1,5 @@
 var appsList = [];
-var LocalNotifications = require("nativescript-local-notifications");
-var createViewModel = require("~/util/NotificationUtil.js").createViewModel;
-var page;
 
 exports.onSelectApp = function(index) {
 
-};
-
-exports.onNavigate = function(args) {
-  page = args.object;
-    LocalNotifications.requestPermission().then((granted) => {
-        if(granted) {
-            console.log("GRANTED");
-            page.bindingContext = createViewModel();
-        }
-    })
-};
-
-exports.onNotify = function() {
-  page.bindingContext.schedule();
 };
