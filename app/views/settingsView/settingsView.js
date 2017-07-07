5f3730deef2b9ab7927b581d421679e6a406b7bb
jamescarroll <carrollj@stanford.edu>
2017-07-07 13:19:02 -0700

    AndroidManifest fix

--- app/views/settingsView/settingsView.js
+++ app/views/settingsView/settingsView.js
@@ -3,9 +3,6 @@ var application = require("application");
 const TrackingService = require("~/util/TrackingService");
 var Toast = require("nativescript-toast");
 var drawerModule = require("nativescript-telerik-ui/sidedrawer");
-var drawer;
-
-var UsageUtil = require("~/util/UsageInformationUtil.js");
 
 // expose native APIs
 var Intent = android.content.Intent;
@@ -15,6 +12,7 @@ var Process = android.os.Process;
 var Context = android.content.Context;
 var ActivityManager = android.app.ActivityManager;
 var Integer = java.lang.Integer;
+var drawer;
 
 // global vars
 var context = application.android.context.getApplicationContext();
@@ -37,6 +35,8 @@ exports.pageLoaded = function(args) {
 	drawer = args.object.getViewById("sideDrawer");
 	if (!checkActionUsagePermission()) {
 		launchActionUsageIntent();
+	} else {
+		context.startService(mServiceIntent);
 	}
 }
 
@@ -60,26 +60,17 @@ exports.toggleDrawer = function() {
 };
 
 exports.getRunningServices = function() {	
-
-	var list = UsageUtil.getApplicationList();
-
-	for (var i = 0; i < list.length; i++) {
-		console.log(list[i].label, list[i].averageUsage, list[i].packageName);
+	console.log("---------------------------------------------");
+	var manager = context.getSystemService(Context.ACTIVITY_SERVICE);
+	var services = manager.getRunningServices(Integer.MAX_VALUE);
+	for (var i = 0; i < services.size(); i++) {
+		var service = services.get(i);
+		if (service.service.getClassName() === com.habitlab.TrackingService.class.getName()) {
+			console.log("=====>", service.service.getClassName());
+		} else {
+			console.log("      ", service.service.getClassName());
+		}	
 	}
-
-
-
-	// console.log("---------------------------------------------");
-	// var manager = context.getSystemService(Context.ACTIVITY_SERVICE);
-	// var services = manager.getRunningServices(Integer.MAX_VALUE);
-	// for (var i = 0; i < services.size(); i++) {
-	// 	var service = services.get(i);
-	// 	if (service.service.getClassName() === com.habitlab.TrackingService.class.getName()) {
-	// 		console.log("=====>", service.service.getClassName());
-	// 	} else {
-	// 		console.log("      ", service.service.getClassName());
-	// 	}	
-	// }
 }
 
 
