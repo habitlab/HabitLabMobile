6af97095537c11ae089bc4833ebc0f29b629499e
jamescarroll <carrollj@stanford.edu>
2017-07-07 09:54:39 -0700

    Minor service updates

--- app/util/TrackingService.js
+++ app/util/TrackingService.js
@@ -1,39 +1,57 @@
+// requires
 var application = require("application");
-var Toast = require("nativescript-toast"); // install nativescript-toast plugin
-var context = application.android.context.getApplicationContext();
+var Toast = require("nativescript-toast");
+var timer = require("timer");
 
 // expose native APIs
 var Context = android.content.Context;
 var System = java.lang.System;
 var Service = android.app.Service;
 var UsageEvents = android.app.usage.UsageEvents;
+var Integer = java.lang.Integer;
 
 // global vars
-var timer = require("timer");
+var context = application.android.context.getApplicationContext();
 var timerID;
 
-
+/*
+ * com.habitlab.TrackingService
+ * ----------------------------
+ * Extension of Android Service class. Overrides onStartCommand
+ * of Service class to start timer that continuously runs in
+ * background to perform some task.
+ */
 android.app.Service.extend("com.habitlab.TrackingService", {
 	onStartCommand: function(intent, flags, startId) {
 		this.super.onStartCommand(intent, flags, startId);
 		startTimer();
+        console.log("SERVICE CREATED");
 		return Service.START_STICKY; 
 	}, 
 
 	onDestroy: function() {
 		this.super.onDestroy();
-		console.log("DESTROYED");
+		console.log("SERVICE DESTROYED");
+        previousPackageName = "";
 	}, 
 
     onCreate: function() {
-        
+        // do nothing
     },
 
     onBind: function(intent) {
-        // console.log("##onBind NOT YET IMPLEMENTED");
+        // do nothing 
     }
 });
 
+
+/*
+ * startTimer
+ * ----------
+ * Initializes a NativeScript timer (if one is not already
+ * active) that runs a function for a predetermined amount 
+ * of time.
+ */
 var startTimer = function() {
 	if (!timerID) {
 		timerID = timer.setInterval(() => {
@@ -42,17 +60,33 @@ var startTimer = function() {
 	}
 }
 
+/**
+ * stopTimer
+ * ---------
+ * Function to kill the existing timer. Called by other 
+ * modules BEFORE the call to stopService (otherwise the 
+ * timer thread will still be active, and the service will
+ * not be properly destroyed).
+ */
 var stopTimer = function() {
 	timer.clearInterval(timerID);
 	timerID = 0;
 }
 
+
+/******* TRACKING FUNCTIONALITY *******/
 var usageStatsManager = context.getSystemService(Context.USAGE_STATS_SERVICE);
 var event = new UsageEvents.Event();
 var previousPackageName = "";
 
 var statsMap = [0, 0, 0, 0];
 
+/*
+ * tracking
+ * --------
+ * Function to be run at predetermined intervals in the 
+ * background.
+ */
 var tracking = function () {
 	var events = usageStatsManager.queryEvents(System.currentTimeMillis() - 100000, System.currentTimeMillis());
 
@@ -82,4 +116,23 @@ var tracking = function () {
     }
 };
 
-module.exports = {stopTimer: stopTimer};
+
+/*
+ * isServiceRunning
+ * ----------------
+ * Checks whether TrackingService is active on the device. Called
+ * from other modules. 
+ */
+var isServiceRunning = function () {
+    var manager = context.getSystemService(Context.ACTIVITY_SERVICE);
+    var services = manager.getRunningServices(Integer.MAX_VALUE);
+    for (var i = 0; i < services.size(); i++) {
+        var service = services.get(i);
+        if (service.service.getClassName() === com.habitlab.TrackingService.class.getName()) {
+            return true;
+        }
+    }
+    return false;
+}
+
+module.exports = {stopTimer: stopTimer, isServiceRunning: isServiceRunning};
--- app/views/settingsView/settingsView.js
+++ app/views/settingsView/settingsView.js
@@ -1,13 +1,12 @@
+// requires
+var application = require("application");
 var usageUtil = require('~/util/UsageInformationUtil.js');
 var permissionUtil = require('~/util/PermissionUtil.js');
 var drawerModule = require("nativescript-telerik-ui/sidedrawer");
 var frameModule = require("ui/frame");
 var view = require("ui/core/view");
-var drawer;
-
+const TrackingService = require("~/util/TrackingService");
 var Toast = require("nativescript-toast"); // install nativescript-toast plugin
-
-var application = require("application");
 
 // expose native APIs
 var Intent = android.content.Intent;
@@ -18,72 +17,51 @@ var Context = android.content.Context;
 var ActivityManager = android.app.ActivityManager;
 var Integer = java.lang.Integer;
 
-
-
-// contexts
-var context = application.android.context.getApplicationContext();
-
-// // custom classes
-const TrackingService = require("~/TrackingService");
-
 // global vars
+var context = application.android.context.getApplicationContext();
 var mServiceIntent = new Intent(context, com.habitlab.TrackingService.class);
+var drawer;
 
+// only here until added to onboarding
 function checkActionUsagePermission() {
 	var appOps = context.getSystemService(Context.APP_OPS_SERVICE);
 	var mode = appOps.checkOpNoThrow(AppOpsManager.OPSTR_GET_USAGE_STATS, Process.myUid(), context.getPackageName());
     return mode === AppOpsManager.MODE_ALLOWED;
 }
 
+// only here until added to onboarding
 function launchActionUsageIntent() {
 	var int = new Intent(Settings.ACTION_USAGE_ACCESS_SETTINGS);
 	application.android.foregroundActivity.startActivity(int);	
 }
 
 exports.pageLoaded = function() {
+	drawer = view.getViewById(page, "sideDrawer");
 	if (!checkActionUsagePermission()) {
 		launchActionUsageIntent();
-	} else {
-		context.startService(mServiceIntent); 
-	}	
+	}
 }
 
 exports.enableService = function() {
-	Toast.makeText("Starting Service").show();
-	if (!isServiceRunning()) {
+	if (!TrackingService.isServiceRunning()) {
+		Toast.makeText("Starting Service").show();
 		context.startService(mServiceIntent);
 	}
 }
 
 exports.disableService = function () {
-	if (isServiceRunning()) {
+	if (TrackingService.isServiceRunning()) {
+		Toast.makeText("Stopping Service").show();
 		TrackingService.stopTimer();
 		context.stopService(mServiceIntent);
-		Toast.makeText("Stopping Service").show();
 	}
-}
-
-exports.getRunningServices = function() {
-	getRunningServices();
 }
 
 exports.toggleDrawer = function() {
     drawer.toggleDrawerState();
 };
 
-var isServiceRunning = function () {
-	var manager = context.getSystemService(Context.ACTIVITY_SERVICE);
-	var services = manager.getRunningServices(Integer.MAX_VALUE);
-	for (var i = 0; i < services.size(); i++) {
-		var service = services.get(i);
-		if (service.service.getClassName() === com.habitlab.TrackingService.class.getName()) {
-			return true;
-		}
-	}
-	return false;
-}
-
-var getRunningServices = function() {	
+exports.getRunningServices = function() {	
 	console.log("---------------------------------------------");
 	var manager = context.getSystemService(Context.ACTIVITY_SERVICE);
 	var services = manager.getRunningServices(Integer.MAX_VALUE);
@@ -96,4 +74,6 @@ var getRunningServices = function() {
 		}	
 	}
 }
+
+
 
