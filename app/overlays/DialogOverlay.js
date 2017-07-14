7a5598e4f015616c0c509cf8cb9f8411b259ca44
jamescarroll <carrollj@stanford.edu>
2017-07-14 16:24:44 -0700

    Other stuff i forgot to push

---
+++ app/overlays/DialogOverlay.js
@@ -0,0 +1,152 @@
+var app = require("application");
+
+// native APIs
+var WindowManager = android.view.WindowManager;
+var Paint = android.graphics.Paint;
+var Button = android.widget.Button;
+var Resources = android.content.res.Resources;
+var Color = android.graphics.Color;
+var Context = android.content.Context;
+var PixelFormat = android.graphics.PixelFormat;
+var Gravity = android.view.Gravity;
+var TextView = android.widget.TextView;
+var BitmapFactory = android.graphics.BitmapFactory;
+var Bitmap = android.graphics.Bitmap;
+var TypedValue = android.util.TypedValue;
+
+
+/******************************
+ *          PAINTS            *                           
+ ******************************/
+
+var iconBkgd = [34, 0.81, 1];
+
+var DIALOG_FILL = new Paint();
+DIALOG_FILL.setColor(Color.WHITE); // default
+
+var DIM_BACKGROUND = new Paint();
+DIM_BACKGROUND.setColor(Color.BLACK);
+
+var ICON_FILL = new Paint();
+ICON_FILL.setColor(Color.HSVToColor(iconBkgd));
+
+// CONSTANTS
+var SCREEN_WIDTH = Resources.getSystem().getDisplayMetrics().widthPixels;
+var SCREEN_HEIGHT = Resources.getSystem().getDisplayMetrics().heightPixels;
+var DIALOG_WIDTH = 0.8 * SCREEN_WIDTH;
+var DIALOG_HEIGHT = 0.3 * SCREEN_HEIGHT;
+var LEFT = 0.1 * SCREEN_WIDTH;
+var RIGHT = LEFT + DIALOG_WIDTH;
+var TOP = (SCREEN_HEIGHT - DIALOG_HEIGHT) / 2;
+var BOTTOM = TOP + DIALOG_HEIGHT;
+var ICON_RADIUS = 0.13 * DIALOG_WIDTH;
+var CORNER_RADIUS = 15;
+
+
+var appCtx = app.android.context;
+
+var textToDisplay = "DISPLAY TEXT";
+
+// Custom DialogView 
+var DialogView = android.view.View.extend({
+	onDraw: function (canvas) {
+		DIM_BACKGROUND.setAlpha(128); // 50% dimness
+		canvas.drawRect(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, DIM_BACKGROUND);
+		canvas.drawRoundRect(LEFT, TOP, RIGHT, BOTTOM, CORNER_RADIUS, CORNER_RADIUS, DIALOG_FILL);
+
+		// add icon
+		var iconLeft = SCREEN_WIDTH / 2 - ICON_RADIUS;
+		var iconRight = iconLeft + 2 * ICON_RADIUS;
+		var iconTop = (SCREEN_HEIGHT - DIALOG_HEIGHT) / 2 - 1.2 * ICON_RADIUS;
+		var iconBottom = iconTop + 2 * ICON_RADIUS;
+		canvas.drawOval(iconLeft, iconTop, iconRight, iconBottom, ICON_FILL);
+
+		var icon_id = appCtx.getResources().getIdentifier("logo_bubbles", "drawable", appCtx.getPackageName());
+		var bitmap = appCtx.getResources().getDrawable(icon_id).getBitmap();
+		var hToWRatio = bitmap.getWidth() / bitmap.getHeight();
+		var newHeight = 1.5 * ICON_RADIUS;
+		var newWidth = newHeight * hToWRatio;
+		var icon = Bitmap.createScaledBitmap(bitmap, newWidth, newHeight, false);
+		
+		var bitmapLeft = iconLeft + (iconRight - iconLeft) / 2 - newWidth / 2;
+		var bitmapTop = iconTop + (iconBottom - iconTop) / 2 - newHeight * 9 / 16;
+
+		canvas.drawBitmap(icon, bitmapLeft, bitmapTop, ICON_FILL);
+	}
+});
+
+
+
+function showPosNegDialogOverlay(context, msg, pos, neg, posCallback, negCallback) {
+	var windowManager = context.getSystemService(Context.WINDOW_SERVICE);
+
+	// add view
+	var viewParams = new WindowManager.LayoutParams(WindowManager.LayoutParams.MATCH_PARENT, 
+		WindowManager.LayoutParams.MATCH_PARENT, WindowManager.LayoutParams.TYPE_SYSTEM_ALERT,
+		WindowManager.LayoutParams.FLAG_FULLSCREEN, PixelFormat.TRANSLUCENT);
+	viewParams.gravity = Gravity.LEFT | Gravity.TOP;
+    var view = new DialogView(context);
+    windowManager.addView(view, viewParams);
+
+    // add text
+    var textParams = new WindowManager.LayoutParams(0.8 * DIALOG_WIDTH, 0.65 * DIALOG_HEIGHT,
+    	0.1 * (SCREEN_WIDTH + DIALOG_WIDTH), 0.36 * SCREEN_HEIGHT, 
+    	WindowManager.LayoutParams.TYPE_SYSTEM_ALERT, 0, PixelFormat.TRANSLUCENT);
+    textParams.gravity = Gravity.LEFT | Gravity.TOP;
+    var textView = new TextView(context);
+    textView.setText(msg);
+    textView.setTextSize(TypedValue.COMPLEX_UNIT_PT, 10);
+    textView.setTextColor(Color.BLACK);
+    textView.setHorizontallyScrolling(false);
+    textView.setGravity(Gravity.CENTER);
+    windowManager.addView(textView, textParams);
+
+    // add positive button
+    var posButtonParams = new WindowManager.LayoutParams(0.35 * DIALOG_WIDTH, 
+    	0.3 * DIALOG_HEIGHT, 0.1 * (SCREEN_WIDTH + DIALOG_WIDTH), 
+    	0.35 * SCREEN_HEIGHT + 0.6 * DIALOG_HEIGHT, WindowManager.LayoutParams.TYPE_SYSTEM_ALERT,
+		WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL, PixelFormat.TRANSLUCENT);
+   	posButtonParams.gravity = Gravity.LEFT | Gravity.TOP;
+    var posButton = new Button(context);
+	posButton.setText(pos);
+	posButton.setOnClickListener(new android.view.View.OnClickListener({
+	    onClick: function() {
+	    	if (posCallback) {
+	    		posCallback();
+	    	}
+	        removeViews();
+	    }
+	}));
+    windowManager.addView(posButton, posButtonParams);
+
+    // add positive button
+    var negButtonParams = new WindowManager.LayoutParams(0.35 * DIALOG_WIDTH, 
+    	0.3 * DIALOG_HEIGHT, 0.1 * SCREEN_WIDTH + 0.55 * DIALOG_WIDTH, 
+    	0.35 * SCREEN_HEIGHT + 0.6 * DIALOG_HEIGHT, WindowManager.LayoutParams.TYPE_SYSTEM_ALERT,
+		WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL, PixelFormat.TRANSLUCENT);
+   	negButtonParams.gravity = Gravity.LEFT | Gravity.TOP;
+    var negButton = new Button(context);
+	negButton.setText(neg);
+	negButton.setOnClickListener(new android.view.View.OnClickListener({
+	    onClick: function() {
+	    	if (negCallback) {
+	    		negCallback();
+	    	}
+	        removeViews();
+	    }
+	}));
+    windowManager.addView(negButton, negButtonParams);
+
+    var removeViews = function () {
+    	windowManager.removeView(view);
+    	windowManager.removeView(textView);
+	    windowManager.removeView(posButton);
+	    windowManager.removeView(negButton);
+    }
+}
+
+module.exports = {
+	showPosNegDialogOverlay
+};
+
+
--- app/views/settingsView/settingsView.js
+++ app/views/settingsView/settingsView.js
@@ -3,9 +3,9 @@ var application = require("application");
 const TrackingService = require("~/services/TrackingService");
 const UnlockService = require("~/services/UnlockService");
 const ServiceManager = require("~/services/ServiceManager");
-
 const PermissionUtil = require("~/util/PermissionUtil");
 const StorageUtil = require("~/util/StorageUtil");
+const DialogOverlay = require("~/overlays/DialogOverlay");
 
 var Toast = require("nativescript-toast");
 var dialogs = require("ui/dialogs");
@@ -56,42 +56,9 @@ exports.toggleDrawer = function() {
 
 
 
-var WindowManager = android.view.WindowManager;
-var Paint = android.graphics.Paint;
-var Button = android.widget.Button;
-var Resources = android.content.res.Resources;
-
-var fill = new Paint();
-fill.setColor(android.graphics.Color.BLACK);
-
-var CustomView = android.view.View.extend({
-	onDraw: function (canvas) {
-		canvas.drawRect(0, 0, Resources.getSystem().getDisplayMetrics().widthPixels, Resources.getSystem().getDisplayMetrics().heightPixels,fill);
-	}
-})
 
 exports.getRunningServices = function() {
-	var windowManager = context.getSystemService(Context.WINDOW_SERVICE);
-
-	// add view
-	var viewParams = new WindowManager.LayoutParams(WindowManager.LayoutParams.MATCH_PARENT, 
-		WindowManager.LayoutParams.MATCH_PARENT, WindowManager.LayoutParams.TYPE_SYSTEM_OVERLAY,
-		0, android.graphics.PixelFormat.TRANSLUCENT);
-	viewParams.gravity = android.view.Gravity.LEFT | android.view.Gravity.TOP;
-    viewParams.setTitle("Load Average");
-    var view = new CustomView(context);
-    windowManager.addView(view, viewParams);
-
-    // add button
-    var buttonParams = new WindowManager.LayoutParams(WindowManager.LayoutParams.WRAP_CONTENT, 
-		WindowManager.LayoutParams.WRAP_CONTENT, WindowManager.LayoutParams.TYPE_SYSTEM_OVERLAY,
-		0, android.graphics.PixelFormat.TRANSLUCENT);
-	buttonParams.gravity = android.view.Gravity.LEFT | android.view.Gravity.TOP;
-    var button = new Button(context);
-	button.setText("Push Me!");
-    windowManager.addView(button, buttonParams);
-
-	// ServiceManager.getRunningServices();
+	ServiceManager.getRunningServices();
 };
 
 
