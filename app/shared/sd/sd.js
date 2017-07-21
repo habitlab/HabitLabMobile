ab7adcf8a61c2b2c6d5607cb10f84475769d882b
Sara <svalderr@stanford.edu>
2017-07-20 18:53:39 -0700

    menu stuff

--- app/shared/sd/sd.js
+++ app/shared/sd/sd.js
@@ -1,24 +1,47 @@
 var frameModule = require("ui/frame");
+var menu;
+var selected;
+var options = ['progress-option', 'goals-option', 'settings-option', 'nudges-option', 'feedback-option'];
+
+exports.onLoaded = function(args) {
+  menu = args.object;
+};
+
+var clearSelected = function() {
+  options.forEach(function (item) {
+    menu.getViewById(item).className = "side-option";
+  });
+};
 
 exports.goToProgress = function() {
+  clearSelected();
+  menu.getViewById('progress-option').className = "side-option selected-option";
   frameModule.topmost().navigate("views/progressView/progressView");
 };
 
 
 exports.goToGoals = function() {
+  clearSelected();
+  menu.getViewById('goals-option').className = "side-option selected-option";
   frameModule.topmost().navigate("views/goalsView/goalsView");
 };
 
 
 exports.goToSettings = function() {
+  clearSelected();
+  menu.getViewById('settings-option').className = "side-option selected-option";
   frameModule.topmost().navigate("views/settingsView/settingsView");
 };
 
 
-exports.goToInterventions = function() {
+exports.goToNudges = function() {
+  clearSelected();
+  menu.getViewById('nudges-option').className = "side-option selected-option";
   frameModule.topmost().navigate("views/interventionsView/interventionsView");
 };
 
-exports.goToContact = function() {
-  frameModule.topmost().navigate("views/contactView/contactView");
+exports.goToFeedback = function() {
+  clearSelected();
+  menu.getViewById('feedback-option').className = "side-option selected-option";
+  frameModule.topmost().navigate("views/feedbackView/feedbackView");
 }
