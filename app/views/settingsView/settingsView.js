d5265e5ff2d44b7b6f54714748034e1c8d3c0c41
jamescarroll <carrollj@stanford.edu>
2017-07-25 14:31:41 -0700

    added more icons, changes to settings view

--- app/views/settingsView/settingsView.js
+++ app/views/settingsView/settingsView.js
@@ -1,4 +1,5 @@
 var application = require("application");
+var frame = require("ui/frame");
 
 const TrackingService = require("~/services/TrackingService");
 const UnlockService = require("~/services/UnlockService");
@@ -14,25 +15,14 @@ exports.reset = function() {
 	StorageUtil.setUp();
 };
 
-exports.enableServices = function() {
-	var foregroundActivity = application.android.foregroundActivity;
-	var int = new android.content.Intent(android.provider.Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
-	var uri = android.net.Uri.fromParts("package", application.android.context.getPackageName(), null);
-	int.setData(uri);
-	foregroundActivity.startActivity(int);
-};
-
-exports.disableServices = function () {
-	
-};
 
 exports.toggleDrawer = function() {
 	drawer.toggleDrawerState();
 };
 
 
-exports.getRunningServices = function() {
-	ServiceManager.getRunningServices();
+exports.goToFAQ = function () {
+	frame.topmost().navigate("views/faqView/faqView");
 };
 
 
