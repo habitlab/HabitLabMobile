1b8061ecd1b782b2e78ebf22146639a90a4cd871
jamescarroll <carrollj@stanford.edu>
2017-07-26 13:34:46 -0700

    minor change to app.js (go to onboarding)

--- app/app.js
+++ app/app.js
@@ -2,11 +2,11 @@ var applicationModule = require("application");
 var StorageUtil = require('~/util/StorageUtil');
 var Toast = require("nativescript-toast");
 
-// var view = 'onboardingView';
-// if (StorageUtil.isSetUp()) {
-//   view = 'progressView';
-// }
-view = "appsView";
+var view = 'onboardingView';
+if (StorageUtil.isSetUp()) {
+  view = 'progressView';
+}
+
 applicationModule.start({ 
   moduleName: "views/" + view + "/" + view, 
   backstackVisible: view === 'progressView'
