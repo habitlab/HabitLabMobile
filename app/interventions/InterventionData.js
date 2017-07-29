dbd8c41641f8a9a7d6d02e633fd3f6a85fa96dbc
Sara <svalderr@stanford.edu>
2017-07-28 17:56:26 -0700

    summaries done

--- app/interventions/InterventionData.js
+++ app/interventions/InterventionData.js
@@ -27,35 +27,40 @@ exports.interventionDetails = [
   functionality: "Glances Notification", 
   description: "Sends a notification telling you how many times you've glanced at your phone today", 
   target: 'phone', 
-  level: 'medium'
+  level: 'medium',
+  summary: 'Sends notification with your glance count for the day'
 }, { // 1
   name: 'Knock Knock', 
   icon: 'res://ic_smile', 
   functionality: "Unlocks Toast", 
   description: "Sends a disappearing message telling you how many times you've unlocked your phone today", 
   target: 'phone', 
-  level: 'easy'
+  level: 'easy',
+  summary: 'Sends pop up with your glance count for the day'
 }, { // 2
   name: 'Hello, Old Friend', 
   icon: 'res://ic_hand', 
   functionality: "Unlocks Notification", 
   description: "Sends a notification telling you how many times you've unlocked your phone today", 
   target: 'phone', 
-  level: 'medium'
+  level: 'medium',
+  summary: 'Sends notification with your unlock count for the day'
 }, { // 3
   name: 'Progress Report', 
   icon: 'res://ic_clipboard', 
   functionality: "App Usage Toast", 
-  description: "Sends a disappearing message telling you how long you've been on your phone today", 
+  description: "Sends a disappearing message telling you how long you've been on a specific app today", 
   target: 'app', 
-  level: 'easy'
+  level: 'easy',
+  summary: "Sends pop up with today's total usage for a certain app"
 }, { // 4
   name: 'Red Alert!', 
   icon: 'res://ic_alert', 
   functionality: "App Usage Notification", 
   description: "Sends a notification telling you how long you've been on your phone today", 
   target: 'app', 
-  level: 'medium'
+  level: 'medium',
+  summary: "Sends notification with today's total usage for a certain app"
 }, { // 5
   name: 'How Time Flies!', 
   icon: 'res://ic_plane', 
@@ -63,27 +68,30 @@ exports.interventionDetails = [
   description: "Sends a disappearing message when you are on a watchlisted app telling you how long that visit has been", 
   target: 'app', 
   level: 'easy',
-  summary: 'Pop up message with current visit duration'
+  summary: 'Sends a pop up message with current app visit duration'
 }, { // 6
   name: 'The Clock is Ticking', 
   icon: 'res://ic_hourglass', 
   functionality: "Visit Length Notification", 
   description: "Sends a notification when you are on a watchlisted app telling you how long that visit has been", 
-  level: 'medium'
+  level: 'medium',
+  summary: 'Sends a notification with current app visit duration'
 }, { // 7
   name: 'At it Again', 
   icon: 'res://ic_history', 
   functionality: "Visits Toast", 
   description: "Sends a disappearing message telling you how many times you've been on a certain app today", 
   target: 'app', 
-  level: 'easy'
+  level: 'easy',
+  summary: 'Sends a pop up with your app visit count'
 }, { // 8
   name: 'Repeat Offender', 
   icon: 'res://ic_repeat', 
   functionality: "Visits Notification", 
   description: "Sends a notification telling you how many times you've been on a certain app today", 
   target: 'app', 
-  level: 'medium'
+  level: 'medium',
+  summary: 'Sends a notification with your app visit count'
 }, { // 9
   name: 'Block-Buster', 
   icon: 'res://ic_videocam', 
@@ -91,54 +99,62 @@ exports.interventionDetails = [
   description: "Pauses YouTube and Facebook videos until you confirm that you would like to continue watching", 
   target: 'app', 
   level: 'medium', 
-  apps: ['com.facebook.katana', 'com.google.android.youtube']
+  apps: ['com.facebook.katana', 'com.google.android.youtube'],
+  summary: 'Asks for confirmation before playing videos'
 }, { // 10
   name: 'No Peeking!', 
   icon: 'res://ic_key', 
   functionality: "Full Screen Overlay", 
   description: "Covers the screen when you enter a watchlisted app and prompts you to either continue or exit the app", 
   target: 'app', 
-  level: 'easy'
+  level: 'easy',
+  summary: 'Asks for confirmation before opening watchlisted apps'
 }, { // 11
   name: 'Counting on You', 
   icon: 'res://ic_clock', 
   functionality: "Timer Counting Up Overlay", 
-  description: "Puts a timer in the bottom right corner of your screen counting how long you spend in an app", 
+  description: "Puts a flick-to-move timer in the bottom right corner of your screen counting how long you spend in an app", 
   target: 'app', 
-  level: 'easy'
+  level: 'easy',
+  summary: 'Puts a timer on screen in watchlisted apps'
 }, { // 12
   name: 'The Final Countdown', 
   icon: 'res://ic_exclamation', 
   functionality: "Timer Counting Down Overlay", 
-  description: "Puts a timer in the bottom right corner of your screen counting down until it closes the app", 
+  description: "Puts a flick-to-move timer in the bottom right corner of your screen counting down until it closes the app", 
   target: 'app', 
-  level: 'difficult'
+  level: 'difficult',
+  summary: "On screen timer that closes the app when time runs out"
 }, { // 13
   name: '', 
   icon: 'res://ic_eye', 
   functionality: "Unlocks Dialog", 
   description: "", 
   target: 'phone', 
-  level: 'medium'
+  level: 'medium',
+  summary: ''
 }, { // 14
   name: '', 
   icon: 'res://ic_eye', 
   functionality: "Usage Dialog", 
   description: "", 
   target: 'app', 
-  level: 'medium'
+  level: 'medium',
+  summary: ''
 }, { // 15
   name: '', 
   icon: 'res://ic_eye', 
   functionality: "Visit Duration Dialog", 
   description: "", 
   target: 'app', 
-  level: 'medium'
+  level: 'medium',
+  summary: ''
 }, { // 16
   name: '', 
   icon: 'res://ic_eye', 
   functionality: "Visit Count Dialog", 
   description: "", 
   target: 'app', 
-  level: 'medium'
+  level: 'medium',
+  summary: ''
 }];
