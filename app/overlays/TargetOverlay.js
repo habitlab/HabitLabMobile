67191ff0c304f81971445b668a072a966e8decdc
radhajain <jainr@stanford.edu>
2017-08-29 14:10:15 -0700

    Target overlay

--- app/overlays/TargetOverlay.js
+++ app/overlays/TargetOverlay.js
@@ -185,6 +185,189 @@ exports.removeIntroDialog = function () {
 }
 
 
+// CONSTANTS
+
+var TARGET_HEIGHT = 0.4 * SCREEN_HEIGHT;
+var TARGETTOP = (SCREEN_HEIGHT - TARGET_HEIGHT) / 2;
+var TARGETBOTTOM = TARGETTOP + TARGET_HEIGHT;
+var ICON_RADIUS = 0.08 * DIALOG_WIDTH;
+var CORNER_RADIUS = 15;
+
+var HEADER_HEIGHT = 0.3*TARGET_HEIGHT;
+var HEADER_BOTTOM = TARGETTOP + HEADER_HEIGHT;
+
+var DIM_BACKGROUND = new Paint();
+DIM_BACKGROUND.setColor(Color.BLACK);
+
+var ICON_FILL = new Paint();
+ICON_FILL.setColor(Color.parseColor("#ffad74")); //orange
+
+
+
+
+var TARGET_DIALOG_FILL = new Paint();
+TARGET_DIALOG_FILL.setColor(Color.WHITE); // default
+
+
+var TARGET_HEADER_FILL = new Paint();
+TARGET_HEADER_FILL.setColor(Color.parseColor("#FF9F1C")); //yellow
+
+
+var context = app.android.context;
+var windowManager = context.getSystemService(Context.WINDOW_SERVICE);
+
+// Custom DialogView 
+var TargetEnableDialog = android.view.View.extend({
+	onDraw: function (canvas) {
+		DIM_BACKGROUND.setAlpha(128); // 50% dimness
+		canvas.drawRect(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, DIM_BACKGROUND);
+		canvas.drawRoundRect(LEFT, TARGETTOP, RIGHT, TARGETBOTTOM, CORNER_RADIUS, CORNER_RADIUS, TARGET_DIALOG_FILL);
+		canvas.drawRoundRect(LEFT, TARGETTOP, RIGHT, HEADER_BOTTOM, CORNER_RADIUS, CORNER_RADIUS, TARGET_HEADER_FILL);
+
+		// add icon frame
+		var iconLeft = SCREEN_WIDTH / 2 - ICON_RADIUS;
+		var iconRight = iconLeft + 2 * ICON_RADIUS;
+		var iconTop = (SCREEN_HEIGHT - HEADER_HEIGHT) / 2 - 1 * ICON_RADIUS - 0.05*TARGET_HEIGHT;
+		var iconBottom = iconTop + 2 * ICON_RADIUS;
+		// canvas.drawOval(iconLeft - 10, iconTop - 10, iconRight + 10, iconBottom + 10, ICON_BACK_FILL);
+		canvas.drawOval(iconLeft, iconTop, iconRight, iconBottom, ICON_FILL);
+		canvas.drawRect(LEFT, HEADER_BOTTOM - 0.01*TARGET_HEIGHT, RIGHT, HEADER_BOTTOM + 0.01*TARGET_HEIGHT, ICON_FILL);
+
+		// add icon
+		var icon_id = context.getResources().getIdentifier("ic_unlock", "drawable", context.getPackageName());
+		var bitmap = context.getResources().getDrawable(icon_id).getBitmap();
+		var hToWRatio = bitmap.getWidth() / bitmap.getHeight();
+		var newHeight = 1.5 * ICON_RADIUS;
+		var newWidth = newHeight * hToWRatio;
+		var icon = Bitmap.createScaledBitmap(bitmap, newWidth, newHeight, false);
+		
+		var bitmapLeft = iconLeft + (iconRight - iconLeft) / 2 - newWidth / 2;
+		var bitmapTop = iconTop + (iconBottom - iconTop) / 2 - newHeight * 9 / 16;
+
+		canvas.drawBitmap(icon, bitmapLeft, bitmapTop, ICON_FILL);
+	}
+});
+
+
+
+
+var targetDialog;
+var targetTitle;
+var targetText;
+var targetPosButton;
+var targetNegButton;
+exports.showTargetEnableOverlay = function (title, msg, pos, neg, posCallback, negCallback) {
+	// add whole screen view
+	var viewParams = new WindowManager.LayoutParams(WindowManager.LayoutParams.MATCH_PARENT, 
+		WindowManager.LayoutParams.MATCH_PARENT, WindowManager.LayoutParams.TYPE_SYSTEM_ALERT,
+		WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL |
+        WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE, 
+		PixelFormat.TRANSLUCENT);
+	viewParams.gravity = Gravity.LEFT | Gravity.TOP;
+    targetDialog = new TargetEnableDialog(context);
+    windowManager.addView(targetDialog, viewParams);
+
+     // add title
+    var titleParams = new WindowManager.LayoutParams(0.8 * DIALOG_WIDTH, 0.1 * TARGET_HEIGHT,
+    	0.1 * (SCREEN_WIDTH + DIALOG_WIDTH), TARGETTOP + 0.075*TARGET_HEIGHT, 
+    	WindowManager.LayoutParams.TYPE_SYSTEM_ALERT, 0, PixelFormat.TRANSLUCENT);
+    titleParams.gravity = Gravity.LEFT | Gravity.TOP;
+    targetTitle = new TextView(context);
+    targetTitle.setText(title);
+    targetTitle.setTextSize(TypedValue.COMPLEX_UNIT_PT, 10);
+    targetTitle.setTextColor(Color.WHITE);
+    targetTitle.setHorizontallyScrolling(false);
+    targetTitle.setGravity(Gravity.CENTER);
+    windowManager.addView(targetTitle, titleParams);
+
+
+    // add text
+    var textParams = new WindowManager.LayoutParams(0.85 * DIALOG_WIDTH, 0.65 * TARGET_HEIGHT,
+    	LEFT + 0.075 * DIALOG_WIDTH, TARGETTOP+0.22*TARGET_HEIGHT, 
+    	WindowManager.LayoutParams.TYPE_SYSTEM_ALERT, 0, PixelFormat.TRANSLUCENT);
+    textParams.gravity = Gravity.LEFT | Gravity.TOP;
+    targetText = new TextView(context);
+    targetText.setText(msg);
+    targetText.setTextSize(TypedValue.COMPLEX_UNIT_PT, 9);
+    targetText.setTextColor(Color.parseColor("#808080")); //grey
+    targetText.setHorizontallyScrolling(false);
+    targetText.setGravity(Gravity.CENTER);
+    windowManager.addView(targetText, textParams);
+
+    // add positive button
+    var posButtonParams = new WindowManager.LayoutParams(0.4 * DIALOG_WIDTH, 
+    	0.18 * TARGET_HEIGHT, 0.1* SCREEN_WIDTH + 0.067 * DIALOG_WIDTH, 
+    	TARGETTOP+0.75*TARGET_HEIGHT, WindowManager.LayoutParams.TYPE_SYSTEM_ALERT,
+		WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL | 
+		WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE, 
+		PixelFormat.TRANSLUCENT);
+   	posButtonParams.gravity = Gravity.LEFT | Gravity.TOP;
+    targetPosButton = new Button(context);
+    targetPosButton.getBackground().setColorFilter(Color.parseColor("#51b270"), android.graphics.PorterDuff.Mode.MULTIPLY); //green
+	targetPosButton.setText(pos);
+	targetPosButton.setTextColor(Color.WHITE);
+	targetPosButton.setOnClickListener(new android.view.View.OnClickListener({
+	    onClick: function() {
+	        if (posCallback) {
+	    		posCallback();
+	    	}
+	        exports.removeTargetDialog();
+	    }
+	}));
+    windowManager.addView(targetPosButton, posButtonParams);
+
+
+        // add positive button
+    var negButtonParams = new WindowManager.LayoutParams(0.4 * DIALOG_WIDTH, 
+    	0.18 * TARGET_HEIGHT, 0.1 * SCREEN_WIDTH + 0.534 * DIALOG_WIDTH, 
+    	TARGETTOP+0.75*TARGET_HEIGHT, WindowManager.LayoutParams.TYPE_SYSTEM_ALERT,
+		WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL | 
+		WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE, 
+		PixelFormat.TRANSLUCENT);
+   	negButtonParams.gravity = Gravity.LEFT | Gravity.TOP;
+    targetNegButton = new Button(context);
+    targetNegButton.getBackground().setColorFilter(Color.parseColor("#d13b49"), android.graphics.PorterDuff.Mode.MULTIPLY); //red
+	targetNegButton.setText(neg);
+	targetNegButton.setTextColor(Color.WHITE);
+	targetNegButton.setOnClickListener(new android.view.View.OnClickListener({
+	    onClick: function() {
+	        if (negCallback) {
+	    		negCallback();
+	    	}
+	        exports.removeTargetDialog();
+	    }
+	}));
+    windowManager.addView(targetNegButton, negButtonParams);
+}
+
+
+exports.removeTargetDialog = function () {
+	if (targetDialog) {
+		windowManager.removeView(targetDialog);
+		targetDialog = undefined;
+	}
+
+	if (targetTitle) {
+		windowManager.removeView(targetTitle);
+		targetTitle = undefined;
+	}
+
+	if (targetText) {
+		windowManager.removeView(targetText);
+		targetText = undefined;
+	}
+
+	if (targetPosButton) {
+		windowManager.removeView(targetPosButton);
+		targetPosButton = undefined;
+	}
+
+	if (targetNegButton) {
+		windowManager.removeView(targetNegButton);
+		targetNegButton = undefined;
+	}
+}
+
 
 
 
