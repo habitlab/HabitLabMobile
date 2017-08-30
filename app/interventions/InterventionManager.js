627ebea4b06e0b4220b1ab84bd6b9da26d6bb011
jamescarroll <carrollj@stanford.edu>
2017-08-30 15:58:41 -0700

    Cleaned up permission util

--- app/interventions/InterventionManager.js
+++ app/interventions/InterventionManager.js
@@ -638,7 +638,7 @@ var audioFocusListener = new android.media.AudioManager.OnAudioFocusChangeListen
  */
 var showFullScreenOverlay = function (real, pkg) {
   if (!real) {
-    FullScreenOverlay.showOverlay("Continue to Faceook?", 
+    FullScreenOverlay.showOverlay("Continue to Facebook?", 
       "You've already been here 25 times today. Want to take a break?", 
       "Continue to Facebook", "Get me out of here!", null, null);
     return;
