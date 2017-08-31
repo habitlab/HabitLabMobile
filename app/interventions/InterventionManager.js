75d5de9fdf37764c5f694a6eeb3760a5b92492ad
radhajain <jainr@stanford.edu>
2017-08-31 14:03:45 -0700

    Again

--- app/interventions/InterventionManager.js
+++ app/interventions/InterventionManager.js
@@ -838,7 +838,7 @@ var positiveAppToast = function(real, pkg) {
     // add icon
     var icon_id = context.getResources().getIdentifier("ic_habitlab_white", "drawable", context.getPackageName());
     var bitmap = context.getResources().getDrawable(icon_id).getBitmap();
-    ToastOverlay.showToastOverlay("Open HabitLab", bitmap, null);
+    ToastOverlay.showToastOverlay("Open HabitLab", bitmap, null, false);
     return;
   }
 
