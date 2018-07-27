2abcc3a10001af0ac552116524fe0fa8a94ad7ae
DrewGregory <djgregny@gmail.com>
2018-07-27 01:23:03 -0700

    1.4.7

--- app/util/PermissionUtil.js
+++ app/util/PermissionUtil.js
@@ -90,7 +90,7 @@ exports.launchAccessibilityServiceIntent = function () {
  */
 exports.getOverlayType = function(otherOverlay) {
 	if (android.os.Build.VERSION.SDK_INT < 26)
-		return otherOverlay != null ? otherOverlay : otherWindowManager.LayoutParams.TYPE_SYSTEM_ALERT;
+		return otherOverlay != null ? otherOverlay : WindowManager.LayoutParams.TYPE_SYSTEM_ALERT;
 	return WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY;
 }
 
