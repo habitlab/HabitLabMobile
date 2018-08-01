da63248351de9f81cf6ec12ad6ac0681e483e170
DrewGregory <djgregny@gmail.com>
2018-08-01 16:25:07 -0700

    fix arithmetic error.

--- app/overlays/LockdownOverlay.js
+++ app/overlays/LockdownOverlay.js
@@ -18,7 +18,7 @@ var LayoutParams = android.view.ViewGroup.LayoutParams;
 
 
 /******************************
- *          PAINTS            *                           
+ *          PAINTS            *
  ******************************/
 
 
@@ -30,7 +30,7 @@ MAIN.setColor(Color.parseColor("#db646f"));
 var BACKGROUND = new Paint();
 BACKGROUND.setColor(Color.parseColor("#d13b49"));
 
-//Orange for icon fill 
+//Orange for icon fill
 var ICON_FILL = new Paint();
 ICON_FILL.setColor(Color.parseColor("#ffad74"));
 
@@ -44,7 +44,7 @@ var ICON_RADIUS = 0.075 * SCREEN_HEIGHT;
 var context = application.android.context;
 var windowManager = context.getSystemService(Context.WINDOW_SERVICE);
 
-// Custom DialogView 
+// Custom DialogView
 var DialogView = android.view.View.extend({
 	onDraw: function (canvas) {
 		//drawRect(float left, float top, float right, float bottom, Paint paint)
@@ -65,7 +65,7 @@ var DialogView = android.view.View.extend({
 		var newHeight = 1.5 * ICON_RADIUS;
 		var newWidth = newHeight * hToWRatio;
 		var icon = Bitmap.createScaledBitmap(bitmap, newWidth, newHeight, false);
-		
+
 		var bitmapLeft = iconLeft + (iconRight - iconLeft) / 2 - newWidth / 2;
 		var bitmapTop = iconTop + (iconBottom - iconTop) / 2 - newHeight * 9 / 16;
 
@@ -84,110 +84,110 @@ var labelText;
 
 exports.showOverlay = function (title, msg, pos, prog, max, posCallback, negCallback) {
     if (permissions.checkSystemOverlayPermission()) {
-		// add view
-		var viewParams = new WindowManager.LayoutParams(WindowManager.LayoutParams.MATCH_PARENT, 
-			WindowManager.LayoutParams.MATCH_PARENT, permissions.getOverlayType(),
-			WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE, PixelFormat.TRANSLUCENT);
-		viewParams.gravity = Gravity.LEFT | Gravity.TOP;
-		overlayView = new DialogView(context);
-		windowManager.addView(overlayView, viewParams);
+  		// add view
+  		var viewParams = new WindowManager.LayoutParams(WindowManager.LayoutParams.MATCH_PARENT,
+  			WindowManager.LayoutParams.MATCH_PARENT, permissions.getOverlayType(),
+  			WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE, PixelFormat.TRANSLUCENT);
+  		viewParams.gravity = Gravity.LEFT | Gravity.TOP;
+  		overlayView = new DialogView(context);
+  		windowManager.addView(overlayView, viewParams);
 
 
-		// add title
-		var titleParams = new WindowManager.LayoutParams(0.8 * SCREEN_WIDTH, LayoutParams.WRAP_CONTENT,
-			0.1 * SCREEN_WIDTH, 0.3 * SCREEN_HEIGHT, 
-			permissions.getOverlayType(), 0, PixelFormat.TRANSLUCENT);
-		titleParams.gravity = Gravity.LEFT | Gravity.TOP;
-		overlayTitle = new TextView(context);
-		overlayTitle.setText(title);
-		overlayTitle.setTextSize(TypedValue.COMPLEX_UNIT_PT, 14);
-		overlayTitle.setTextColor(Color.WHITE);
-		overlayTitle.setHorizontallyScrolling(false);
-		overlayTitle.setGravity(Gravity.CENTER);
-		windowManager.addView(overlayTitle, titleParams);
+  		// add title
+  		var titleParams = new WindowManager.LayoutParams(0.8 * SCREEN_WIDTH, LayoutParams.WRAP_CONTENT,
+  			0.1 * SCREEN_WIDTH, 0.3 * SCREEN_HEIGHT,
+  			permissions.getOverlayType(), 0, PixelFormat.TRANSLUCENT);
+  		titleParams.gravity = Gravity.LEFT | Gravity.TOP;
+  		overlayTitle = new TextView(context);
+  		overlayTitle.setText(title);
+  		overlayTitle.setTextSize(TypedValue.COMPLEX_UNIT_PT, 14);
+  		overlayTitle.setTextColor(Color.WHITE);
+  		overlayTitle.setHorizontallyScrolling(false);
+  		overlayTitle.setGravity(Gravity.CENTER);
+  		windowManager.addView(overlayTitle, titleParams);
 
-		// add text
-		var textParams = new WindowManager.LayoutParams(0.8 * SCREEN_WIDTH, LayoutParams.WRAP_CONTENT,
-			0.1 * SCREEN_WIDTH, 0.435 * SCREEN_HEIGHT, 
-			permissions.getOverlayType(), 0, PixelFormat.TRANSLUCENT);
-		textParams.gravity = Gravity.LEFT | Gravity.TOP;
-		overlayText = new TextView(context);
-		overlayText.setText(msg);
-		overlayText.setTextSize(TypedValue.COMPLEX_UNIT_PT, 10);
-		overlayText.setTextColor(Color.WHITE);
-		overlayText.setHorizontallyScrolling(false);
-		overlayText.setGravity(Gravity.CENTER);
-		windowManager.addView(overlayText, textParams);
+  		// add text
+  		var textParams = new WindowManager.LayoutParams(0.8 * SCREEN_WIDTH, LayoutParams.WRAP_CONTENT,
+  			0.1 * SCREEN_WIDTH, 0.435 * SCREEN_HEIGHT,
+  			permissions.getOverlayType(), 0, PixelFormat.TRANSLUCENT);
+  		textParams.gravity = Gravity.LEFT | Gravity.TOP;
+  		overlayText = new TextView(context);
+  		overlayText.setText(msg);
+  		overlayText.setTextSize(TypedValue.COMPLEX_UNIT_PT, 10);
+  		overlayText.setTextColor(Color.WHITE);
+  		overlayText.setHorizontallyScrolling(false);
+  		overlayText.setGravity(Gravity.CENTER);
+  		windowManager.addView(overlayText, textParams);
 
-		//Progress bar 
-		var progParams = new WindowManager.LayoutParams(0.8 * SCREEN_WIDTH, LayoutParams.WRAP_CONTENT,
-			0, 0.1*SCREEN_HEIGHT, 
-			permissions.getOverlayType(), 0, PixelFormat.TRANSLUCENT);
-		progBar = new ProgressBar(context, null, android.R.attr.progressBarStyleHorizontal);
-		progBar.setMax(max);
-		progBar.setProgress(prog);
-		windowManager.addView(progBar, progParams);
+  		//Progress bar
+  		var progParams = new WindowManager.LayoutParams(0.8 * SCREEN_WIDTH, LayoutParams.WRAP_CONTENT,
+  			0, 0.1*SCREEN_HEIGHT,
+  			permissions.getOverlayType(), 0, PixelFormat.TRANSLUCENT);
+  		progBar = new ProgressBar(context, null, android.R.attr.progressBarStyleHorizontal);
+  		progBar.setMax(max);
+  		progBar.setProgress(prog);
+  		windowManager.addView(progBar, progParams);
 
-		//Time label
-		var labelParams = new WindowManager.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT,
-			0.35*SCREEN_WIDTH, 0.12 * SCREEN_HEIGHT, 
-			permissions.getOverlayType(), 0, PixelFormat.TRANSLUCENT);
-		labelText = new TextView(context);
-		labelText.setText(max + " mins");
-		labelText.setTextSize(TypedValue.COMPLEX_UNIT_PT, 5);
-		labelText.setTextColor(Color.WHITE);
-		labelText.setHorizontallyScrolling(false);
-		windowManager.addView(labelText, labelParams);
+  		//Time label
+  		var labelParams = new WindowManager.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT,
+  			0.35*SCREEN_WIDTH, 0.12 * SCREEN_HEIGHT,
+  			permissions.getOverlayType(), 0, PixelFormat.TRANSLUCENT);
+  		labelText = new TextView(context);
+  		labelText.setText(max + " mins");
+  		labelText.setTextSize(TypedValue.COMPLEX_UNIT_PT, 5);
+  		labelText.setTextColor(Color.WHITE);
+  		labelText.setHorizontallyScrolling(false);
+  		windowManager.addView(labelText, labelParams);
 
-		//Add exit button
-		var linkParams = new WindowManager.LayoutParams(0.8 * SCREEN_WIDTH, LayoutParams.WRAP_CONTENT,
-			0.1 * SCREEN_WIDTH, 0.75 * SCREEN_HEIGHT, 
-			permissions.getOverlayType(), WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL, 
-			PixelFormat.TRANSLUCENT);
-		linkParams.gravity = Gravity.LEFT | Gravity.TOP;
-		overlayLink = new TextView(context);
-		overlayLink.setText("Turn Off Lockdown Mode");
-		overlayLink.setTextSize(TypedValue.COMPLEX_UNIT_PT, 5);
-		overlayLink.setTextColor(Color.WHITE);
-		overlayLink.setHorizontallyScrolling(false);
-		overlayLink.setGravity(Gravity.CENTER);
-		overlayLink.setOnClickListener(new android.view.View.OnClickListener({
-			onClick: function() {
-				if (negCallback) {
-					negCallback();
-				}
-				// exports.removeOverlay();
-			}
-		}));
-		windowManager.addView(overlayLink, linkParams);
+  		//Add exit button
+  		var linkParams = new WindowManager.LayoutParams(0.8 * SCREEN_WIDTH, LayoutParams.WRAP_CONTENT,
+  			0.1 * SCREEN_WIDTH, 0.75 * SCREEN_HEIGHT,
+  			permissions.getOverlayType(), WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL,
+  			PixelFormat.TRANSLUCENT);
+  		linkParams.gravity = Gravity.LEFT | Gravity.TOP;
+  		overlayLink = new TextView(context);
+  		overlayLink.setText("Turn Off Lockdown Mode");
+  		overlayLink.setTextSize(TypedValue.COMPLEX_UNIT_PT, 5);
+  		overlayLink.setTextColor(Color.WHITE);
+  		overlayLink.setHorizontallyScrolling(false);
+  		overlayLink.setGravity(Gravity.CENTER);
+  		overlayLink.setOnClickListener(new android.view.View.OnClickListener({
+  			onClick: function() {
+  				if (negCallback) {
+  					negCallback();
+  				}
+  				// exports.removeOverlay();
+  			}
+  		}));
+  		windowManager.addView(overlayLink, linkParams);
 
-		// add positive button
-		var posButtonParams = new WindowManager.LayoutParams(0.6 * SCREEN_WIDTH, 
-			0.08 * SCREEN_HEIGHT, 0.2 * SCREEN_WIDTH, 0.65 * SCREEN_HEIGHT, 
-			permissions.getOverlayType(),
-			WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL | 
-			WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE, 
-			PixelFormat.TRANSLUCENT);
-		posButtonParams.gravity = Gravity.LEFT | Gravity.TOP;
-		overlayPosButton = new Button(context);
-		overlayPosButton.setText(pos);
-		overlayPosButton.setTextColor(Color.parseColor("#d13b49"));
-		overlayPosButton.getBackground().setColorFilter(Color.parseColor("#eeeeeeff"), android.graphics.PorterDuff.Mode.MULTIPLY);
-		overlayPosButton.setOnClickListener(new android.view.View.OnClickListener({
-			onClick: function() {
-				if (posCallback) {
-					posCallback();
-				}
-				exports.removeOverlay();
-			}
-		}));
-		windowManager.addView(overlayPosButton, posButtonParams);
+  		// add positive button
+  		var posButtonParams = new WindowManager.LayoutParams(0.6 * SCREEN_WIDTH,
+  			0.08 * SCREEN_HEIGHT, 0.2 * SCREEN_WIDTH, 0.65 * SCREEN_HEIGHT,
+  			permissions.getOverlayType(),
+  			WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL |
+  			WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
+  			PixelFormat.TRANSLUCENT);
+  		posButtonParams.gravity = Gravity.LEFT | Gravity.TOP;
+  		overlayPosButton = new Button(context);
+  		overlayPosButton.setText(pos);
+  		overlayPosButton.setTextColor(Color.parseColor("#d13b49"));
+  		overlayPosButton.getBackground().setColorFilter(Color.parseColor("#eeeeeeff"), android.graphics.PorterDuff.Mode.MULTIPLY);
+  		overlayPosButton.setOnClickListener(new android.view.View.OnClickListener({
+  			onClick: function() {
+  				if (posCallback) {
+  					posCallback();
+  				}
+  				exports.removeOverlay();
+  			}
+  		}));
+  		windowManager.addView(overlayPosButton, posButtonParams);
 
     } else {
         permissions.launchSystemOverlayIntent(); 
     }
-	
-    
+
+
 }
 
 exports.removeOverlay = function () {
@@ -226,4 +226,3 @@ exports.removeOverlay = function () {
 		labelText = undefined;
 	}
 }
-
